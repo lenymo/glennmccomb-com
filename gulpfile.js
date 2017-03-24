07b2235e235b1682d169274c2482227b59472f15
Glenn McComb <RitzMo@Glenns-MacBook-Pro.local>
2017-03-24 21:47:13 +1100

    Moved admin SCSS into main /src directory and integrated it into main gulp workflow.

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,21 @@ gulp.task('scss', function () {
     .pipe(gulp.dest('static/css'));
 });
 
+// Compile admin SCSS files to CSS
+gulp.task('admin-scss', function () {
+  gulp.src('src/admin/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle : 'expanded'}))
+    .pipe(autoprefixer({
+      browsers : ['last 20 versions']
+    }))
+    .pipe(gulp.dest('static/admin/css'));
+});
+
 // Watch asset folder for changes
-gulp.task('watch', ['scss'], function () {
+gulp.task('watch', ['scss', 'admin-scss'], function () {
   gulp.watch('src/scss/**/*', ['scss']);
+  gulp.watch('src/admin/scss/**/*', ['admin-scss']);
 });
 
 // Set watch as default task
