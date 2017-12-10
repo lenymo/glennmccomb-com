7254ec6e99ea15fb54c7b5a8a875cf4b78d359d0
Glenn McComb <lenymo@gmail.com>
2017-12-10 14:56:38 +1100

    Fixed bug when running ToC JS on pages without a ToC.

--- src/js/_table-of-contents.js
+++ src/js/_table-of-contents.js
@@ -22,19 +22,22 @@ var TableOfContents = (function() {
 
     // console.log( tocToggle );
 
-    // Listen for clicks on the ToC toggle.
-    tocToggle.addEventListener('click', function(e){
-      
-      // If the ToC is expanded.
-      if ( toc.classList.contains( tocToggleClass ) ) {
-        toc.classList.remove( tocToggleClass );
+    // If the ToC toggle exists on the page.
+    if ( tocToggle !== null && tocToggle.length > 0 ) {
+      // Listen for clicks on the ToC toggle.
+      tocToggle.addEventListener('click', function(e){
+        
+        // If the ToC is expanded.
+        if ( toc.classList.contains( tocToggleClass ) ) {
+          toc.classList.remove( tocToggleClass );
 
-      // If the ToC is not expanded.
-      } else {
-        toc.classList.add( tocToggleClass );
-      }
-    });
-  }
+        // If the ToC is not expanded.
+        } else {
+          toc.classList.add( tocToggleClass );
+        }
+      });
+    } // if ( tocToggle.length > 0 )
+  } // handleTableOfContents()
 
 
   //
