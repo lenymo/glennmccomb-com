744dee76fab16090f4d70e4ed65915097cf57bc3
Glenn McComb <lenymo@gmail.com>
2018-04-21 19:48:36 +1000

    Trying last.fm api request again.

--- lambda/hello.js
+++ lambda/hello.js
@@ -151,29 +151,29 @@ function handler(event, context, callback) {
     var lastFmUrl = 'https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=' + username + '&api_key=' + apiKey + '&format=json&period=' + period + '&limit=' + limit;
 
     // I used this to test that the last.fm URL was working.
-    callback(null, {
-      // headers: {
-      //   'Content-Type': 'application/json',
-      //   'Accept': 'application/json',
-      //   "Access-Control-Allow-Origin" : "*",
-      //   "Access-Control-Allow-Credentials" : true
-      // },
-      statusCode: 200,
-      body: JSON.stringify({ lastFmUrl: lastFmUrl })
-    });
+    // callback(null, {
+    //   // headers: {
+    //   //   'Content-Type': 'application/json',
+    //   //   'Accept': 'application/json',
+    //   //   "Access-Control-Allow-Origin" : "*",
+    //   //   "Access-Control-Allow-Credentials" : true
+    //   // },
+    //   statusCode: 200,
+    //   body: JSON.stringify({lastFmUrl: lastFmUrl})
+    // });
 
-    // fetch(lastFmUrl)
-    // .then(response => response.json())
-    //   .then(response => {
-    //     callback(null, {
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         'Accept': 'application/json'
-    //       },
-    //       statusCode: 200,
-    //       body: response
-    //     });
-    //   });
+    fetch(lastFmUrl).then(function (response) {
+      return response.json();
+    }).then(function (response) {
+      callback(null, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        statusCode: 200,
+        body: response
+      });
+    });
   } catch (e) {
     callback(null, {
       statusCode: 500,
