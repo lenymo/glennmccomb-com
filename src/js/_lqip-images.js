a5e4b787b6a42208f89d06716e3d7da6c5a55574
Glenn McComb <lenymo@gmail.com>
2019-09-14 13:19:55 +1000

    Use LQIP for photography single; create new photography-image partial; make lqip JS generic; add generic classes for lqip images; add inline styles to lqip elems (blur filters)

--- src/js/_lqip-images.js
+++ src/js/_lqip-images.js
@@ -1,51 +1,45 @@
-
-
 //
 //  LQIP IMAGES
 //––––––––––––––––––––––––––––––––––––––––––––––––––
 
 var LqipImages = (function() {
-
-
   //
   //  HANDLE LQIP IMAGES
   //––––––––––––––––––––––––––––––––––––––––––––––––––
 
   function handleLqipImages() {
-
     var lqipImages;
     var lqipImage;
     var lqipImageWrapper;
-    var imgShortcode;
+    var lqipFigure;
     var fullImageSrc;
     var fullImage;
-    var fullImageClass = 'img-shortcode__img-full'
-    var imageLoadingClass = '-image-is-loading';
-    var imageLoadedClass = '-image-has-loaded';
-
+    var lqipImagesSelector = ".lqip";
+    var fullImageClass = "lqip__full-image";
+    var imageLoadingClass = "-image-is-loading";
+    var imageLoadedClass = "-image-has-loaded";
 
     // On page load.
     window.onload = function() {
-
       // Find all LQIP images on the page.
-      lqipImages = document.querySelectorAll('.img-shortcode__img-lqip');
+      lqipImages = document.querySelectorAll(lqipImagesSelector);
 
       // Loop through all LQIP images.
       for (var i = 0; i < lqipImages.length; i++) {
-
         // Instantiate this individual image.
         lqipImage = lqipImages[i];
 
+        // Get the lqip image wrapper (.lqip__wrapper).
         lqipImageWrapper = lqipImage.parentElement;
 
-        // Get the images parent (.img-shortcode).
-        imgShortcode = lqipImageWrapper.parentElement;
+        // Get the images parent (.lqip__figure).
+        lqipFigure = lqipImageWrapper.parentElement;
 
         // Add the loading class to the image shortcode.
-        imgShortcode.classList.add( imageLoadingClass );
+        lqipFigure.classList.add(imageLoadingClass);
 
-        // Get the full image src.
-        fullImageSrc = imgShortcode.dataset.full;
+        // Get the full image src from the figure's data-full attr.
+        fullImageSrc = lqipFigure.dataset.full;
 
         // Create a new image.
         fullImage = null;
@@ -53,27 +47,24 @@ var LqipImages = (function() {
 
         // When the full image has loaded.
         fullImage.onload = function() {
-
           lqipImageWrapper = this.parentElement;
-          imgShortcode = lqipImageWrapper.parentElement;
+          lqipFigure = lqipImageWrapper.parentElement;
 
           // Add the loaded class to the full image.
-          imgShortcode.classList.add( imageLoadedClass );
-        }
+          lqipFigure.classList.add(imageLoadedClass);
+        };
 
         // Add the src to the full image.
         fullImage.src = fullImageSrc;
 
         // Add the appropriate BEM clas to the full image.
-        fullImage.classList.add( fullImageClass );
+        fullImage.classList.add(fullImageClass);
 
         // Insert the full image before the LQIP image.
-        lqipImageWrapper.insertBefore( fullImage, lqipImage );
-
+        lqipImageWrapper.insertBefore(fullImage, lqipImage);
       }
-    }
+    };
   } // handleLqipImages()
-
 
   //
   //  INIT
